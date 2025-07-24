77c9af3075db52051d80e14775b4c8f1797bc7a9
sesha10052000 <seshasairammellempudi@gmail.com>
2025-07-24 23:32:51 +0530

    read js commit

--- readItem.js
+++ readItem.js
@@ -10,22 +10,3 @@ window.addEventListener('DOMContentLoaded', async () => {
       message.textContent = "No items found.";
       return;
     }
-
-    data.forEach(item => {
-      const row = document.createElement('tr');
-
-      row.innerHTML = `
-        <td>${item.productName}</td>
-        <td>€${item.price}</td>
-        <td>${item.quantity}</td>
-        <td>${item.sellerName}</td>
-        <td>${item.sellerPhone}</td>
-        <td>${item.sellerEmail}</td>
-      `;
-
-      tableBody.appendChild(row);
-    });
-  } catch (err) {
-    message.textContent = "Error loading items.";
-  }
-});
